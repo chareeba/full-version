6caba10f127f6033717401f32ddc5a799132271d
areeba <areebatariq961@gmail.com>
2025-11-19 09:39:11 -0800

    added all migrations

---
+++ database/migrations/2025_11_19_170314_order.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->integer('order_number');
+            $table->decimal('total_amount', 10, 2);
+            $table->decimal('discount');
+            $table->integer('shipping_cost');
+            $table->string('payment_status')->default("pending");
+            $table->string('order_status')->default("pending");
+            $table->integer('shipping_address_id');
+            $table->string('payment_method')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2025_11_19_170506_product_variant.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_product_variants', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->string('attribute'); // e.g., 'Size'
+            $table->string('value');     // e.g., 'Large'
+            $table->decimal('price_adjustment', 10, 2)->default(0);
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_variants');
+    }
+};
