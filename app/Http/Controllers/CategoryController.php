98af98d590276139f558b5a2b44bca4cee1a2777
areeba <areebatariq961@gmail.com>
2025-11-23 21:06:58 -0800

    added categorycontroller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\models\Category;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -11,7 +11,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $title = "categories";
+        $description = "List of categories";
+        return view('admin.category.index', compact('title', 'description'));
     }
 
     /**
@@ -19,7 +21,10 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+
+        $title = "categories";
+        $description = "List of categories";
+        return view('admin.category.create', compact('title', 'description'));
     }
 
     /**
@@ -27,21 +32,20 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-         $validated = $request->validate([
+        $validated = $request->validate([
             // validation rules
-            'name'=>'required|string|max:255|min:5',
-            'slug'=>'required|string|max:255|min:5|unique:categories,slug',
-            'description'=>'nullable|string',
-            'status'=>'required|boolean',
-            'image'=>'nullable|image|max:2048',
-         ]);
-              $result=Category::create($validated);
-              if($result){
-              return response()->json(['message'=>'category is created successfully'],201);
-              }
-              else{
-                 return response()->json(['message'=>'category creation failed'],500);
-              }
+            'name' => 'required|string|max:255|min:5',
+            'slug' => 'required|string|max:255|min:5|unique:categories,slug',
+            'description' => 'nullable|string',
+            'status' => 'required|boolean',
+            'image' => 'nullable|image|max:2048',
+        ]);
+        $result = Category::create($validated);
+        if ($result) {
+            return response()->json(['message' => 'category is created successfully'], 201);
+        } else {
+            return response()->json(['message' => 'category creation failed'], 500);
+        }
     }
 
     /**
@@ -57,12 +61,14 @@ class CategoryController extends Controller
      */
     public function edit(string $slug)
     {
-           $Category=Category::where('slug',$slug)->firstorFail();
-         if($Category){
-            return response()->json($Category);
-        }
-        else{
-            return response()->json(['message'=>'Category not found'],404);
+
+        $title = "categories";
+        $description = "List of categories";
+        $Category = Category::where('slug', $slug)->firstorFail();
+        if ($Category) {
+            return view('admin.category.edit', compact('title', 'description', 'Category'));
+        } else {
+            return response()->json(['message' => 'Category not found'], 404);
         }
     }
 
@@ -71,14 +77,14 @@ class CategoryController extends Controller
      */
     public function update(Request $request, string $slug)
     {
-         $validated = $request->validate([
+        $validated = $request->validate([
             // validation rules
-            'name'=>'nullable|string|max:255|min:5',
-            'slug'=>'nullable|string|max:255|min:5|unique:categories,slug',
-            'description'=>'nullable|string',
-            'status'=>'boolean',
-            'image'=>'nullable|image|max:2048',
-         ]);
+            'name' => 'nullable|string|max:255|min:5',
+            'slug' => 'nullable|string|max:255|min:5|unique:categories,slug',
+            'description' => 'nullable|string',
+            'status' => 'boolean',
+            'image' => 'nullable|image|max:2048',
+        ]);
     }
 
     /**
@@ -86,13 +92,12 @@ class CategoryController extends Controller
      */
     public function destroy(string $slug)
     {
-            $Category=Category::where('slug',$slug)->firstorFail();
-            if($Category){
-                $Category->delete();
-                return response()->json(['message'=>'Category deleted successfully'],200);
-            }
-            else{
-                return response()->json(['message'=>'Category not found'],404);
-            }
+        $Category = Category::where('slug', $slug)->firstorFail();
+        if ($Category) {
+            $Category->delete();
+            return response()->json(['message' => 'Category deleted successfully'], 200);
+        } else {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
     }
 }
