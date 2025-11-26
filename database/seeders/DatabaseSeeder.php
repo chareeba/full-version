d0ef7261913b4f022ac03c5986fcd0ffb01f8010
areeba <areebatariq961@gmail.com>
2025-11-25 23:23:49 -0800

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,6 +31,8 @@ class DatabaseSeeder extends Seeder
             OrderItemSeeder::class,
             ReviewSeeder::class,
             SettingSeeder::class,
+            WishlistSeeder::class,
+            PaymentSeeder::class,
         ]);
     }
 }
