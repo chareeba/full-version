47d6e3ba1f4d9769f2b3f609d4fe10efe4843ab0
areeba <areebatariq961@gmail.com>
2025-11-26 23:10:52 -0800

    added relations

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -21,15 +21,28 @@ class Customer extends Model
         'status'
     ];
     protected $casts = [
-        'first_name'  => 'string',
-        'last_name'   => 'string',
-        'email'       => 'string',
-        'phone'       => 'string',
-        'address'     => 'string',
-        'city'        => 'string',
-        'state'       => 'string',
-        'country'=> 'string',
+        'first_name' => 'string',
+        'last_name' => 'string',
+        'email' => 'string',
+        'phone' => 'string',
+        'address' => 'string',
+        'city' => 'string',
+        'state' => 'string',
+        'country' => 'string',
         'postal_code' => 'string',
-        'status'      => 'boolean', // active / inactive
+        'status' => 'boolean', // active / inactive
     ];
+
+    // Relationships
+    public function orders()
+    {
+        // If you add a `customer_id` on orders, this will return related orders
+        return $this->hasMany(Order::class, 'customer_id');
+    }
+
+    public function reviews()
+    {
+        // If reviews are made by customers (instead of users), this will return them
+        return $this->hasMany(Review::class, 'user_id');
+    }
 }
